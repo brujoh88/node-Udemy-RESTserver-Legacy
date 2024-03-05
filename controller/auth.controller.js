c936af84944a846e53cd30b63c27239ccd498c25
Gus - MVC <gustavotiseira@gmail>
2024-03-05 12:14:43 -0300

    Clase 149 Login

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,8 +1,40 @@
 const { response } = require('express')
-const login = (req, res = response)=>{
-    res.json({
-        msg:'Login ok'
-    })
+const Usuario = require('../models/usuarios')
+const bcryptjs = require('bcryptjs')
+
+const login = async (req, res = response)=>{  
+
+    const {correo, password } = req.body
+    try {
+
+        const usuario = await Usuario.findOne({correo})
+        if (!usuario) {
+        return res.status(400).json({
+            msg: "Usuario/Password no son correctos - EMAIL"})
+        }
+
+        if (!usuario.estado) {
+            return res.status(400).json({
+                msg: "Usuario/Password no son correctos - Estado: false"})
+        }
+
+        const valirdPassword = bcryptjs.compareSync(password, usuario.password)
+        if (!valirdPassword) {
+            return res.status(400).json({
+                msg: "Usuario/Password no son correctos - Password"})
+        }
+
+
+        res.json({
+            msg:'Login ok'
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            msg:'Hable con el administrador'
+        })
+    }    
 }
 module.exports = {
     login
