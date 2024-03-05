fd81dcb33ab0085849c0eb434b86866c2ce84570
Gus - MVC <gustavotiseira@gmail>
2024-03-05 12:37:46 -0300

    Clase 150 generar JWT

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,6 +1,7 @@
 const { response } = require('express')
 const Usuario = require('../models/usuarios')
 const bcryptjs = require('bcryptjs')
+const { generarJWT } = require('../helpers/generarJWT')
 
 const login = async (req, res = response)=>{  
 
@@ -23,10 +24,14 @@ const login = async (req, res = response)=>{
             return res.status(400).json({
                 msg: "Usuario/Password no son correctos - Password"})
         }
+        
+
+        const token = await generarJWT(usuario.id)
 
 
         res.json({
-            msg:'Login ok'
+            usuario,
+            token
         })
 
     } catch (error) {
