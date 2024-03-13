fb6971e753c6373e0d4936b397ddc49c885636d6
Gus - MVC <gustavotiseira@gmail>
2024-03-13 13:50:45 -0300

    Clase 185 Busqueda por coleccion

--- controller/buscar.controller.js
+++ controller/buscar.controller.js
@@ -1,10 +1,48 @@
 const { response, request } = require("express")
-
-
+const { Usuario, Categoria, Producto } = require("../models")
+const { ObjectId } = require('mongoose').Types
+const coleccionesPermitidas =  [
+    'categoria',
+    'productos',
+    'roles',
+    'usuarios',
+]
+const buscarUsuario = async (termino = '',res = response )=>{
+    const esMongoId = ObjectId.isValid(termino)
+    if (esMongoId) {
+        const usuario = await Usuario.findById(termino)
+        return res.json({
+            results:usuario?[usuario]:[]
+        })
+    }
+}
 const buscar = (req = request, res = response )=>{
-    res.json({
-        msg:'Buscar...'
-    })
+
+    const {coleccion, termino} = req.params
+
+    if (!coleccionesPermitidas.includes( coleccion )) {
+        return res.status(400).json({
+            msg: `Las colecciones permitidas son : ${coleccionesPermitidas}`
+        })
+        
+    }
+
+    switch (coleccion) {
+        case 'categoria':        
+            
+            break;
+        case 'productos':
+            break;
+
+        case 'usuarios':
+            buscarUsuario(termino,res)
+            break;
+    
+        default:
+
+            break;
+    }
+    
 }
 
 module.exports = {
