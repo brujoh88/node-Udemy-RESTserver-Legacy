7b51a239624b60a09e06053978569c2e0202d466
Gus - MVC <gustavotiseira@gmail>
2024-03-15 08:41:24 -0300

    Clase 197 helper subirArchivo

--- controller/upload.controller.js
+++ controller/upload.controller.js
@@ -1,38 +1,18 @@
-const path =  require('path')
-const { v4: uuidv4 } = require('uuid')
 const { response, request } = require("express");
+const { subirArchivo } = require('../helpers');
 
 
 
-const cargarArchivo = (req = request, res = response)=>{  
+const cargarArchivo = async(req = request, res = response)=>{  
 
   if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
     res.status(400).json({msg:'No hay archivo en la peticion'});
     return;
   }
-
-  const { archivo } = req.files;
-
-   const nombreCortado = archivo.name.split('.')
-   const extension = nombreCortado[ nombreCortado.length - 1 ]
-
-
-   const extensionesValidas = ['png','jpg','gif']
-
-   if (!extensionesValidas.includes( extension )) {
-    return res.status(400).json({msg:`La extension ${extension} no es permitida. Extensiones validas: ${extensionesValidas}`})    
-   }
-
-  const nombreTemp = uuidv4() + '.' + extension
-
-  const uploadPath = path.join( __dirname,  '../uploads/' + nombreTemp)
-
-  archivo.mv(uploadPath, (err)=> {
-    if (err) {
-      return res.status(500).json({err});
-    }
-    res.json({msg: 'File uploaded to ' + uploadPath});
-  });
+  const nombre = await subirArchivo  (req.files)
+  res.json({
+    nombre
+  })
 }
 
 
