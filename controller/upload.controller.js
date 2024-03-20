686106c45f6ac846991c81744a2a8679b9b391d6
Gus - MVC <gustavotiseira@gmail>
2024-03-20 09:10:09 -0300

    Clase 206 Carga de imagenes a Cloudinay

--- controller/upload.controller.js
+++ controller/upload.controller.js
@@ -3,8 +3,8 @@ const { subirArchivo } = require('../helpers');
 const { Usuario, Producto } = require("../models");
 const path = require('path')
 const fs = require('fs')
-
-
+const cloudinary = require('cloudinary').v2
+cloudinary.config(process.env.CLOUDINARY_URL);
 const cargarArchivo = async(req = request, res = response)=>{    
   try {
     //const nombre = await subirArchivo  (req.files, ['txt','md'], 'textos')
@@ -96,8 +96,47 @@ const mostrarImagen = async (req= request, res= response)=>{
   res.json({msg:'falta placeholder'})
 }
 
+const actualizarImagenCloudinary  = async (req = request, res = response)=>{  
+  const {id,coleccion} = req.params
+  let modelo
+  switch (coleccion) {
+    case 'usuarios':
+      modelo = await Usuario.findById(id)
+      if (!modelo) {
+        return res.status(400).json({msg:`No existe el usuario con ID: ${id}`})
+      }
+      break;
+    
+      case 'productos':
+        modelo = await Producto.findById(id)
+        if (!modelo) {
+          return res.status(400).json({msg:`No existe el producto con ID: ${id}`})
+        }
+        break;
+  
+    default:
+      return res.status(500).json({msg:'Se me olvido validar esto'})
+  }
+
+  try {
+    if (modelo.img) {
+      
+    }
+  } catch (error) {
+    
+  }
+
+  const { tempFilePath } = req.files.archivo
+  const { secure_url } = await cloudinary.uploader.upload(tempFilePath)
+  modelo.img = secure_url
+  await modelo.save()
+  
+  res.json(modelo)
+}
+
 module.exports = {
     actualizarImagen,
+    actualizarImagenCloudinary,
     cargarArchivo,
     mostrarImagen
 }
--- routes/upload.routes.js
+++ routes/upload.routes.js
@@ -1,7 +1,7 @@
 const { Router } = require('express')
 const { check } = require('express-validator')
 const { validarCampos } = require('../middleware/validar-campos')
-const { cargarArchivo, actualizarImagen, mostrarImagen } = require('../controller/upload.controller')
+const { cargarArchivo, actualizarImagen, mostrarImagen, actualizarImagenCloudinary } = require('../controller/upload.controller')
 const { coleccionesPermitidas } = require('../helpers')
 const { validarArchivoSubir } = require('../middleware')
 
@@ -13,7 +13,9 @@ router.put('/:coleccion/:id',[
     check('id','El id debe ser un id de Mongo').isMongoId(),
     check('coleccion').custom(coleccion=>coleccionesPermitidas(coleccion,['usuarios','productos'])),
     validarCampos
-],actualizarImagen)
+],/* actualizarImagen */
+actualizarImagenCloudinary)
+
 
 router.get('/:coleccion/:id',[
     check('id','El id debe ser un id de Mongo').isMongoId(),
